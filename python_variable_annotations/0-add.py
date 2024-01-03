65757207f8c96de6426f489948b91d2c88f4086b
carlalap <carlos3_alarcon@hotmail.com>
2024-01-02 16:07:06 -0800

    sintax again

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -3,8 +3,7 @@
 
 
 def add(a: float, b: float) -> float:
-   """Write a type-annotated function
-    add that takes a float a and a float b as arguments and returns
-    their sum as a float. 
+    """type-annotated function add that takes a float a and a float b
+    as arguments and returns their sum as a float. 
     """
-   return(a + b);
+    return(a + b)
