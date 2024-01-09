8fc25b4016067d8d821f77d6bc1983f548425930
carlalap <carlos3_alarcon@hotmail.com>
2024-01-09 12:49:28 -0800

    style file task0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """task0. Simple helper function"""
 
+
 def index_range(page, page_size):
     """Function that takes two integer arguments page and page_size.
     The function should return a tuple of size two containing a start index
-    and an end index corresponding to the range of indexes to return in a list 
+    and an end index corresponding to the range of indexes to return in a list
     for those particular pagination parameters."""
     if page and page_size:
         start_index = (page - 1) * page_size
