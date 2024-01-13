d803ed4047b332d9b112faf118058dc13e0c9fbc
carlalap <carlos3_alarcon@hotmail.com>
2024-01-13 14:14:26 -0800

    task4. Read and filter data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -69,3 +69,33 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=os.environ.get("PERSONAL_DATA_DB_USERNAME"),
         password=os.environ.get("PERSONAL_DATA_DB_PASSWORD"),
     )
+
+
+def main():
+    """function that obtain a database connection using get_db and
+    retrieve all rows in the users table and display each row under
+    a filtered format like this"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    result = cursor.fetchall()
+    for row in result:
+        info = (
+            f"name={row[0]};"
+            + + f"email={row[1]}; "
+            + f"phone={row[2]}; "
+            + f"ssn={row[3]}; "
+            + f"password={row[4]};"
+        )
+
+        print(info)
+        log_record = logging.LogRecord(
+            "my_logger", logging.INFO, None, None, message, None, None
+        )
+        formatter = RedactingFormatter(PII_FIELDS)
+        formatter.format(log_record)
+    cursor.close()
+    db.close()
+
+    if __name__ == "__main__":
+        main()
