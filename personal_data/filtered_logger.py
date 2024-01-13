d753eb5770e62273009f2f8ca0368de5c1a4b18b
carlalap <carlos3_alarcon@hotmail.com>
2024-01-13 12:01:29 -0800

    task1. Log formatter added

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """0. Regex-ing"""
 import re
+import logging
 from typing import List
 
 
@@ -14,3 +15,22 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          + redaction + separator, message)
 
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """constructor"""
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Method to filter values in incoming log records """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
