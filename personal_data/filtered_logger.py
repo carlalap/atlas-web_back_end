89f770e3f8dbdc1d068d660fb01f3a1a2fc083b6
carlalap <carlos3_alarcon@hotmail.com>
2024-01-13 12:57:41 -0800

    task2. Create logger added

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
-"""0. Regex-ing"""
+"""Tasks: 0.Regex-ing, 1.Log formatter, 2.Create logger,
+3.Connect to secure database, 4.Read and filter data"""
 import re
 import logging
+import os
 from typing import List
+
+
+"""containing the fields from user_data.csv that are considered PII."""
+PII_FIELDS = (
+    "name",
+    "email",
+    "phone",
+    "ssn",
+    "password",
+)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -13,7 +25,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     for items in fields:
         message = re.sub(items + "=.+?(?=abc)*\\" + ";", items + "="
                          + redaction + separator, message)
-
     return message
 
 
@@ -34,3 +45,15 @@ class RedactingFormatter(logging.Formatter):
         """Method to filter values in incoming log records """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """function that takes no arguments
+    and returns a logging.Logger object."""
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(RedactingFormatter(PII_FIELDS)))
+    logger.addHandler(handler)
+    return logger
