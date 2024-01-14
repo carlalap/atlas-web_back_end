f98da161360e2d91bd7c508ad7c6006c40b8b8b8
carlalap <carlos3_alarcon@hotmail.com>
2024-01-14 10:14:42 -0800

    task6. Check valid password added

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """task5. Encrypting passwords &
+task6. Check valid password
 """
 import bcrypt
 
@@ -9,3 +10,13 @@ def hash_password(password: str) -> bytes:
     name password and returns a salted, hashed password,
     which is a byte string."""
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Function that expects 2 arguments (hashed_password & password)
+    and returns a boolean"""
+    encoded_passw = bytes(password, 'utf-8')
+    if bcrypt.checkpw(encoded_passw, hashed_password) is True:
+        return True
+    else:
+        return False
