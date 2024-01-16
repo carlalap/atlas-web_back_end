999c92b4969d30aa17ef6f6444423e4f1cd62537
carlalap <carlos3_alarcon@hotmail.com>
2024-01-16 14:01:24 -0800

    task9. Basic - User credentials added

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -33,3 +33,19 @@ class BasicAuth(Auth):
             return None
 
         return decoded_bytes.decode("utf-8")
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """method that returns the user email and password
+        from the Base64 decoded value"""
+        if (
+            decoded_base64_authorization_header is None or not
+            isinstance(decoded_base64_authorization_header, str)
+        ):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
