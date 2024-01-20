340d93ff539a8bd00d71162bb147a39e59686a2e
carlalap <carlos3_alarcon@hotmail.com>
2024-01-19 16:52:37 -0800

    task1. Empty session added

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -17,10 +17,12 @@ auth = None
 if getenv("AUTH_TYPE") == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-
 elif getenv("AUTH_TYPE") == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif getenv("AUTH_TYPE") == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
@@ -30,39 +32,40 @@ def before_request() -> str:
     if auth is None:
         return None
 
-    check_pathlist = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+    check_pathlist = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if not (auth.require_auth(request.path, check_pathlist)):
         return
-
     if not auth.authorization_header(request)\
        and not auth.session_cookie(request):
         abort(401)
-    request.current_user = auth.current_user(request)
     if request.current_user is None:
         abort(403)
-
-
-@app.errorhandler(404)
-def not_found(error):
-    """ Not found handler
-    """
-    return jsonify({"error": "Not found"}), 404
+    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(401)
-def unauthorized(error):
+def unauthorized(error) -> str:
     """ Task1. Error handler: Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error):
+def forbidden(error) -> str:
     """ task2. Error handler: Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
