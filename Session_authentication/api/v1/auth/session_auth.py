6f68babe1cc6c36e00d8c2ff49ec13ec924a57f4
carlalap <carlos3_alarcon@hotmail.com>
2024-01-21 14:04:46 -0800

    update method User ID for Session ID, file session_auth

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-SessionAuth Class
+SessionAuth Class, creates new authentication
 """
 from api.v1.auth.auth import Auth
 import uuid
@@ -21,11 +21,8 @@ class SessionAuth(Auth):
         SessionAuth.user_id_by_session_id[session_id] = user_id
         return session_id
 
-
-def user_id_for_session_id(self, session_id: str = None) -> str:
-    """Method that hat returns a User ID based on a Session ID"""
-    if session_id is None:
-        return None
-    if type(session_id) is not str:
-        return None
-    return SessionAuth.user_id_by_session_id.get(session_id)
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Method that hat returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
