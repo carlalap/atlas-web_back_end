a5075af5e7ba66a2276dcba96e9d4c00b030d569
carlalap <carlos3_alarcon@hotmail.com>
2024-01-21 15:36:49 -0800

    task7. New view for Session Authentication

--- Session_authentication/api/v1/views/__init__.py
+++ Session_authentication/api/v1/views/__init__.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
+"""Script that sets up a Flask Blueprint (app_views) 
+with a specified URL prefix, 
+-imports views from different modules, 
+-loads user data from a file using the User class. 
 """
 from flask import Blueprint
 
@@ -7,5 +10,7 @@ app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
 from api.v1.views.index import *
 from api.v1.views.users import *
+from api.v1.views.session_auth import *
+
 
 User.load_from_file()
--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -1,1 +1,39 @@
 #!/usr/bin/env python3
+"""Flask view that handles all
+routes for the Session authentication."""
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def auth_session_login() -> str:
+    """Method that handle session authentication login"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    user = User.search({'email': email})
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    session_id = auth.create_session(user[0].id)
+    user_data = user[0].to_json()
+
+    response = jsonify(user_data)
+    response.set_cookie(
+        auth.SESSION_NAME,
+        session_id,
+        httponly=True,
+        secure=True if app_views.app.config['ENV'] == 'production' else False
+    )
+
+    return response
