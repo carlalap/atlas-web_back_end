24cbf33c6dfe5c5324d51076a3256aadabb96063
carlalap <carlos3_alarcon@hotmail.com>
2024-01-22 12:18:58 -0800

    7. Register user added

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,30 @@
 """Flask app
 with a single GET route ("/") that returns
 a JSON payload using flask.jsonify:"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route("/")
 def welcome():
+    """Return a Welcome message"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/user', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """Method implement the end-point to register a user."""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
