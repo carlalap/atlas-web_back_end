c03b816e18cb8ffabea5bcdfae21cc7618d892dc
carlalap <carlos3_alarcon@hotmail.com>
2024-01-22 13:11:52 -0800

    8. Credentials validation added

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -39,3 +39,13 @@ class Auth:
             return reg_user
         else:
             raise ValueError('User {} already exists'.format(email))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Method that check login using email and checking the
+        password bcrypt.checkpw """
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
