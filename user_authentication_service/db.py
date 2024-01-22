59daceb4b6e5cd67789da398aec0010a05685241
carlalap <carlos3_alarcon@hotmail.com>
2024-01-22 08:35:32 -0800

    update task1

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Method that save a user to the DB and returns a User object
         """
         new_user = User(email=email, hashed_password=hashed_password)
