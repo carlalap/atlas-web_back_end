971ac277cee42f16aa7e75a65b535706c6f6b0a6
carlalap <carlos3_alarcon@hotmail.com>
2024-01-22 10:24:06 -0800

    task5. Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,9 +3,39 @@
 Auth Module
 """
 import bcrypt
+from db import DB
+from uuid import uuid4
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> str:
     """Method that takes in a password string
     arguments and returns bytes."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """ Generate a random uuid """
+    return str(uuid4())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Method that register a new user
+        take mandatory email and password
+        string arguments and return a User object."""
+        try:
+            reg_user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            passw = _hash_password(password)
+            reg_user = self._db.add_user(email, passw)
+            return reg_user
+        else:
+            raise ValueError('User {} already exists'.format(email))
