334ca83bc9654ab1234fa06ae41a3fc87fb6cff0
carlalap <carlos3_alarcon@hotmail.com>
2024-01-21 19:23:53 -0800

    update views/session file

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -3,6 +3,7 @@
 from flask import abort, jsonify, request
 from models.user import User
 from api.v1.views import app_views
+from os import getenv
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -24,6 +25,8 @@ def auth_session_login() -> str:
     if not user[0].is_valid_password(password):
         return jsonify({"error": "wrong password"}), 401
 
+
+    from api.v1.app import auth
     session_id = auth.create_session(user[0].id)
     user_data = user[0].to_json()
 
