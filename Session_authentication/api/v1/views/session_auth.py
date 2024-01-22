e6d9e3fd7eff25e7ce28b373fdda999851e8700f
carlalap <carlos3_alarcon@hotmail.com>
2024-01-21 17:58:11 -0800

    update views/session_auth.py

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,17 +7,14 @@ from models.user import User
 from os import getenv
 
 
-
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def auth_session_login() -> str:
+def login() -> str:
     """Method that handle session authentication login
     with the person email and password"""
     email_per = request.form.get('email')
-    password_per = request.form.get('password')
-
     if not email_per:
         return jsonify({"error": "email missing"}), 400
-
+    password_per = request.form.get('password')
     if not password_per:
         return jsonify({"error": "password missing"}), 400
 
@@ -27,10 +24,10 @@ def auth_session_login() -> str:
 
     for user in users:
         if user.is_valid_password(password_per):
-             from api.v1.app import auth
-             session_id = auth.create_session(user.id)
-             user_json = jsonify(user.to_json())
-             user_json.set_cookie(getenv('SESSION_NAME'), session_id)
-             return user_json
+            from api.v1.app import auth
+            session_id = auth.create_session(user.id)
+            user_json = jsonify(user.to_json())
+            user_json.set_cookie(getenv('SESSION_NAME'), session_id)
+            return user_json
         else:
             return jsonify({"error": "wrong password"}), 401
