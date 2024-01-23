800719e6e3cdcb6d14b4ab66d0f61b17763b1692
carlalap <carlos3_alarcon@hotmail.com>
2024-01-23 14:41:59 -0800

    task14 again updated

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -52,9 +52,10 @@ def logout():
     session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user is None:
-        abort(403)
+        return jsonify({"error": "Invalid session ID"}), 403
+
     AUTH.destroy_session(user)
-    return redirect(/)
+    return redirect('/')
 
 
 @app.route('/profile', methods=['GET'])
