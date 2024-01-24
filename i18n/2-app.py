53aa6bc110eac72b65199e7c2d44afd827aaab5f
carlalap <carlalap>
2024-01-24 04:36:56 -0600

    task2

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -3,7 +3,6 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
 app = Flask(__name__)
 babel = Babel(app)
 
@@ -11,11 +10,17 @@ babel = Babel(app)
 class Config():
     """Class to configure language"""
     LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-app.config.from_object('2-app.Config')
-Babel.default_locale = 'en'
-Babel.default_timezone = 'UTC'
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine the best match with our supported languages."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
@@ -23,14 +28,6 @@ def my_home():
     """Method template that simply outputs a message"""
     return render_template('./2-index.html')
 
-
-def get_locale():
-    """get_locale Determine the best match with
-    our supported languages."""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-babel = Babel(app, locale_selector=get_locale)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
