a53d53a00e0923a4489935feef0e038a4a030476
carlalap <carlalap>
2024-01-24 04:08:14 -0600

    Get locale from request added

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """task2. Get locale from request"""
+from locale import getlocale
 from flask import Flask, render_template, request
 from flask_babel import Babel
-from babel import get_locale
+
 
 app = Flask(__name__)
 babel = Babel(app)
@@ -18,7 +19,7 @@ BABEL_DEFAULT_TIMEZONE = "UTC"
 app.config.from_object(Config)
 
 
-@babel.localeselector
+
 def get_locale():
     """Determine the best match with
     our supported languages."""
@@ -27,12 +28,10 @@ def get_locale():
 
     return best_match
 
+babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def my_home():
     """Method template that simply outputs a message"""
-    return render_template('./0-index.html')
-
-
-if __name__ == '__main__':
-    app.run()
+    return render_template('./2-index.html')
