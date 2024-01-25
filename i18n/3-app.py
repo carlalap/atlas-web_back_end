f16e322752f98556c8c37b7dabdf251815f93291
carlalap <carlalap>
2024-01-24 22:26:44 -0600

    reverse changes 3-app file

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,39 +1,33 @@
 #!/usr/bin/env python3
-""" Create a basic Flask App
-    with a single '/' route and an index.html template
-"""
+"""Babel locale from request"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
-
 app = Flask(__name__)
-# Instantiate Babel object in module-level variable babel
 babel = Babel(app)
 
 
 class Config():
-    """ Configure available languages in our app """
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    """Class to configure language"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Use Config class as config for our app
 app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
-    """ Return user preferred locale, if not available return best match """
+    """Determine the best match with our supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def index():
-    """ Return index.html template """
-    from flask_babel import gettext as _
+@app.route('/')
+def my_home():
+    """Method template that simply outputs a message"""
     return render_template('./3-index.html')
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
