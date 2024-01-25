1e4bd686adfa6d737a239d3d6bfa7d1adeaaad42
carlalap <carlalap>
2024-01-24 23:57:36 -0600

    updated 6-app.py file

--- i18n/6-app.py
+++ i18n/6-app.py
@@ -11,8 +11,7 @@ users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
     3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": "None", "timezone": "Europe/Spain"},
-    42: {"name": "Boy", "locale": "fr", "timezone": "Europe/Paris"},
+    4: {"name": "Teletubby", "locale": "None", "timezone": "Europe/Londo"},
 }
 
 
