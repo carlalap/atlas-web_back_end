df24565c0faa0c1070f81440be1d01990c9abb8d
carlalap <carlos3_alarcon@hotmail.com>
2024-01-28 09:51:07 -0800

    task updated

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -9,18 +9,17 @@ class TestAccessNestedMap(unittest.TestCase):
     """Test Suit AccessNestedMap."""
     """ to test the function for following inputs """
 
-    @parameterized.expand(
-        [
-            {"a": 1}, path=("a",)
-            {"a": {"b": 2}}, path=("a",)
-            {"a": {"b": 2}}, path=("a", "b")
-        ])
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
     def test_access_nested_map(self, nested_map, path, expected_result):
         """Method to test that the method returns what it is supposed to."""
         self.assertEqual(access_nested_map(nested_map, path),
                          expected_result)
 
     @parameterized.expand([
-            ({}, ["a"], KeyError),
-            ({"a": 1}, ["a", "b"], KeyError)
+            ({}, ["a"]),
+            ({"a": 1}, ["a", "b"])
         ])
