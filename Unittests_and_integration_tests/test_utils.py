d9736497e4115253a976eb71a767a62c6d8bd9f2
carlalap <carlos3_alarcon@hotmail.com>
2024-01-28 09:47:04 -0800

    task0 added

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,19 +2,25 @@
 """Unittests"""
 import unittest
 from parameterized import parameterized
-from utils import access_nested_map, get_json
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Test Suit AccessNestedMap."""
+    """ to test the function for following inputs """
 
+    @parameterized.expand(
+        [
+            {"a": 1}, path=("a",)
+            {"a": {"b": 2}}, path=("a",)
+            {"a": {"b": 2}}, path=("a", "b")
+        ])
+    def test_access_nested_map(self, nested_map, path, expected_result):
+        """Method to test that the method returns what it is supposed to."""
+        self.assertEqual(access_nested_map(nested_map, path),
+                         expected_result)
 
-@parameterized.expand(
-    [
-        {"a": 1}, path=("a",)
-        {"a": {"b": 2}}, path=("a",)
-        {"a": {"b": 2}}, path=("a", "b")
-    ])
-def test_access_nested_map(self, nested_map, path, expected_result):
-    self.assertEqual(access_nested_map(nested_map, path),
-                     expected_result)
+    @parameterized.expand([
+            ({}, ["a"], KeyError),
+            ({"a": 1}, ["a", "b"], KeyError)
+        ])
