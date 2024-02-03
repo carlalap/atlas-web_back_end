5722b9de31f681d341caebf0d510c8a8ab3ae2d8
carlalap <carlos3_alarcon@hotmail.com>
2024-02-03 09:59:41 -0800

    2task. Incrementing values added

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -3,6 +3,23 @@
 import redis
 import uuid
 from typing import Union, Optional, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Method takes a single method Callable
+    argument and returns a Callable."""
+    key = method.__qualname__
+
+    @wraps(method)
+    def wrapper(self, *args, **kwds):
+        """increments the count for that key every time
+        the method is called and returns the value
+        returned by the original method."""
+        self._redis.incr(key)
+        return method(self, *args, **kwds)
+
+        return wrapper
 
 
 class Cache:
@@ -37,8 +54,5 @@ class Cache:
 
     def get_int(self, key: str) -> int:
         """Get the value from Redis and convert to int"""
-        data = self._redis.get(key)
-        try:
-            return int(data) if data is not None else None
-        except (ValueError, TypeError):
-            return None
+        # data = self._redis.get(key)
+        return int(data)
