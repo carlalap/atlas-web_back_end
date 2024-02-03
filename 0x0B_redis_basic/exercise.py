f82eec6d357f79f720621373912e9e96f88efecd
carlalap <carlos3_alarcon@hotmail.com>
2024-02-02 19:30:47 -0800

    task1. Reading from Redis and recovering original type added

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,3 +18,22 @@ class Cache:
         # Store the data in Redis using the random key
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None)\
+            -> Union[str, bytes, int, float]:
+        """Take a key string argument to convert later
+        the data back to the desired format."""
+        data = self._redis.get(key)
+        if fn:
+            return fn(data)
+        else:
+            return data
+
+    def get_str(self, data):
+        """Get the value from Redis using
+        the get method with automatic conversion"""
+        return data.decode("utf-8")
+
+    def get_int(self, data):
+        """Get the value from Redis and convert to int"""
+        return int(data)
