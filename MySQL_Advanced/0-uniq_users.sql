05a4387af4338e600dadd4e277a8d1422bee154b
carlalap <carlos3_alarcon@hotmail.com>
2024-02-04 12:59:10 -0800

    task0. We are all unique!

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,10 @@
+-- Creates a table called users with the next requirements
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email VARCHAR(255) NOT NULL UNIQUE,
+       name VARCHAR(255)
+ );
