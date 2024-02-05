09891d32001542f2dbabc47536378c0f42fa71ea
carlalap <carlos3_alarcon@hotmail.com>
2024-02-05 08:51:37 -0800

    typing error 5-valid_email.sql fixed

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,15 +1,14 @@
 -- 5. Email validation to sent
 -- SQL script that creates a trigger that resets
 -- the attribute valid_email only when the email has been changed.
-
-DELIMETER //
+DELIMITER //
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
-BEGIN
-    IF NEW.email <> OLD.email THEN
-        SET NEW.valid_email = NULL;
-    END IF;
-END;
+    BEGIN
+        IF NEW.email <> OLD.email THEN
+            SET NEW.valid_email = 0;
+        END IF;
+    END;
 //
-DELIMETER ;
+DELIMITER ;
