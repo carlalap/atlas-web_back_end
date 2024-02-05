6ff4b5485f74ff4900552656108ffc8a95785d6c
carlalap <carlos3_alarcon@hotmail.com>
2024-02-04 18:22:51 -0800

    task2.ranks country origins of bands

---
+++ MySQL_Advanced/2-fans.sql
@@ -0,0 +1,7 @@
+-- SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
+SELECT origin,
+    sum(fans) as nb_fans
+from metal_bands
+group by origin
+order by nb_fans desc;
