7a3f4b562208b65991ce3598f677931d5be6da98
carlalap <carlos3_alarcon@hotmail.com>
2024-02-05 09:26:34 -0800

    task6. Add bonus added

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,7 +1,7 @@
 -- 5. Email validation to sent
 -- SQL script that creates a trigger that resets
 -- the attribute valid_email only when the email has been changed.
-DELIMITER //
+delimiter //
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users
 FOR EACH ROW
@@ -11,4 +11,4 @@ FOR EACH ROW
         END IF;
     END;
 //
-DELIMITER ;
+delimiter ;
