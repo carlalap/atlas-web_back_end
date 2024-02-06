9750b7d06d2b26b34f7778901c2ffd0cec715978
carlalap <carlos3_alarcon@hotmail.com>
2024-02-06 10:16:47 -0800

    10. Safe divide added

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,7 +1,7 @@
 --task10. Safe divide
 -- SQL script that creates a function SafeDiv that divides (and returns)
 -- the first by the second number or returns 0 if the second number is equal to 0.
-delimeter //
+DELIMITER //
 
 CREATE function SafeDiv(a INT, b INT)
 RETURNS INT
@@ -17,4 +17,4 @@ BEGIN
     RETURN result;
 END;
 //
-delimiter;
+DELIMITER;
