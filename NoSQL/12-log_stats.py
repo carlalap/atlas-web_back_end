59de501d33e1b506c824350531d3617017d3830c
carlalap <carlos3_alarcon@hotmail.com>
2024-02-10 15:57:05 -0800

    task12 updated

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,19 +7,20 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """Connecting to MongoDB"""
     client = MongoClient('localhost', 27017)
-    db = client.logs.nginx
+    db = client.logs
+    collection = db.nginx
 
     # Get the total number of documents in the collection
-    count_logs = db.count_documents({})
+    count_logs = collection.count_documents({})
     print(f"{count_logs} logs")
 
     # Get the count of logs for each method
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = db.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print(f"\tmethod {method} = {count}")
 
     # Get the count of logs with method=GET and path=/status
-    count_status = db.count_documents(
+    count_status = collection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{count_status}  status check")
