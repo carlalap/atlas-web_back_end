3ee9d7554e549738f8787d6b663dca7a3f6b6b61
carlalap <carlos3_alarcon@hotmail.com>
2024-02-10 16:10:04 -0800

    task10%12 updated

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """task10. Change school topics """
 
+from typing import List
+
 
 def update_topics(mongo_collection, name, topics):
     """function that changes all topics of a
     school document based on the name"""
-    output = mongo_collection.update_one({"name": name},
+    updated_document = mongo_collection.update_many({"name": name},
                                          {"$set": {"topics": topics}})
-    return output.modified_count
+    return updated_document.modified_count
--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -7,20 +7,19 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """Connecting to MongoDB"""
     client = MongoClient('localhost', 27017)
-    db = client.logs
-    collection = db.nginx
+    DB = client.logs.nginx
 
     # Get the total number of documents in the collection
-    count_logs = collection.count_documents({})
+    count_logs = DB.count_documents({})
     print(f"{count_logs} logs")
 
     # Get the count of logs for each method
     print("Methods:")
     for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method} = {count}")
+        count = DB.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
     # Get the count of logs with method=GET and path=/status
-    count_status = collection.count_documents(
+    count_status = DB.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{count_status}  status check")
