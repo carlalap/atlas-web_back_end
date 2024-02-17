059670dd2da6cbdb6d3ef2775f340a79fb1e60cd
carlalap <carlos3_alarcon@hotmail.com>
2024-02-17 13:28:51 -0800

    1. Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,9 +1,13 @@
 // 1. Using Process stdin
 // program where user should be able
 // to input their name on a new line
-console.log('Welcome to Holberton School, what is your name?')
-process.stdin.once('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    console.log('This important software is now closing');
+process.stdin.setEncoding('utf-8');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
