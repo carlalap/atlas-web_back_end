1fcd030a99bb0c74a3c047eb5bb03e83ccf1fd76
carlalap <carlos3_alarcon@hotmail.com>
2024-02-17 09:38:05 -0800

    basic javascript with Node JS added

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,6 +1,3 @@
-// task0. Executing basic javascript with Node JS
-// function named displayMessage that prints in STDOUT the string argument.
-
 function displayMessage(str) {
     console.log(str);
 }
