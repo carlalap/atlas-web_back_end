5c3392832aa53e711f17c8d7ef339035856d613e
carlalap <carlos3_alarcon@hotmail.com>
2024-02-18 14:14:49 -0800

    task4. Create a small HTTP server added

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -7,10 +7,15 @@ const http = require('http');
 const port = 1245;
 
 const app = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
+  response.writeHead(200, { 'Content-Type': 'text/plain' });
   response.write('Hello Holberton School!'); // print message
   response.end();
 });
-app.listen(port);
+app.listen(port, (error) => {
+  if (error) {
+    console.log('Something went wrong', error);
+  } else {
+    console.log(`Server is listening on port ${port}`);
+  }
+});
 module.exports = app;
