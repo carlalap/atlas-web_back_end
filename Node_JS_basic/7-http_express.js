6bb527a40e92519f6cf812f71324d797a43e01a2
carlalap <carlos3_alarcon@hotmail.com>
2024-02-20 08:13:34 -0800

    task7 HTTP server using Express added

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,31 @@
+// TASK7.  Create a more complex HTTP server using Express
+// create a small HTTP server using Express module:
+// It should be assigned to the variable app and this one must be exported
+// HTTP server should listen on port 1245
+// Displays Hello Holberton School! in the page body for the endpoint /
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (request, response) => response.send('Hello Holberton School!'));
+
+app.get('/students', async (request, response) => {
+  const message = 'This is the list of our students\n';
+  try {
+    const students = await countStudents(process.argv[2]);
+    response.send(`${message}${students.join('\n')}`);
+  } catch (error) {
+    response.send(`${message}${error.message}`);
+  }
+});
+
+app.listen(port, (error) => { // Start the server and listen on the specified port
+  if (error) { // If an error occurs while starting the server
+    console.log('Something went wrong', error); // Log the error message
+  } else { // If the server starts successfully
+    console.log(`Server is listening on port ${port}`); // Log a message indicating that the server is listening on the specified port
+  }
+});
+module.exports = app; // Export the app variable
