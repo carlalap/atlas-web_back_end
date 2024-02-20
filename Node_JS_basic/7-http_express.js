cb293834c7c6ab9d54c3be9bd673ba4fbd1c96fb
carlalap <carlos3_alarcon@hotmail.com>
2024-02-20 09:16:40 -0800

    task7 HTTP server using Express eslint updated

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -61,10 +61,10 @@ app.get('/students', (request, response) => {
 });
 
 app.listen(port, (error) => { // Start the server and listen on the specified port
-    if (error) { // If an error occurs while starting the server
-      console.log('Something went wrong', error); // Log the error message
-    } else { // If the server starts successfully
-      console.log(`Server is listening on port ${port}`); // Log a message indicating that the server is listening on the specified port
-    }
+  if (error) { // If an error occurs while starting the server
+    console.log('Something went wrong', error); // Log the error message
+  } else { // If the server starts successfully
+    console.log(`Server is listening on port ${port}`); // Log a message indicating that the server is listening on the specified port
+  }
 });
 module.exports = app; // Export the app variable
