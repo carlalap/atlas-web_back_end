60dd04c4baf8c249da1e794cecf7cef6d8cc6de7
carlalap <carlos3_alarcon@hotmail.com>
2024-02-24 10:44:38 -0800

    mocha test task3

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -1,6 +1,7 @@
 const Utils = require('./utils.js');
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
-    console.log(`The total is: $ { Utils.calculateNumber('SUM', totalAmount, totalShipping) }`);
+    const message = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: $ { message }`);
 };
 module.exports = sendPaymentRequestToApi;
--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,15 +1,21 @@
 const sinon = require('sinon');
-const chai = require('chai')
+const chai = require('chai');
 const expect = chai.expect;
-
 
 const sendPaymentRequestToApi = require('./3-payment.js');
 const Utils = require('./utils.js');
 
 describe('sendPaymentRequestToApi', function() {
     it('sendPaymentRequestToApi(100, 20) is the same Utils.calculateNumber(\'SUM\', 100, 20)', function() {
-        sinon.spy(Utils, 'calculateNumber');
+        const spyUtils = sinon.spy(Utils, 'calculateNumber');
+        const spyConsole = sinon.spy(console, 'log');
+
         sendPaymentRequestToApi(100, 20);
-        expect(Utils.calculateNumber.calledOnce).to.be.true;
+        
+        expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+        expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
+
+        Utils.calculateNumber.restore();
+        console.log.restore();
     });
 });
