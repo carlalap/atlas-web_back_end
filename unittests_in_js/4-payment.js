f1a5d1e8831d63d526159bd5888713109f27320a
carlalap <carlos3_alarcon@hotmail.com>
2024-02-24 12:48:17 -0800

    task4. Stubs added

---
+++ unittests_in_js/4-payment.js
@@ -0,0 +1,7 @@
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const message = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    console.log(`The total is: $ { message }`);
+};
+module.exports = sendPaymentRequestToApi;
