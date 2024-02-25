f29d161d28b9deb0191f58dacb580e09f1f90a70
carlalap <carlos3_alarcon@hotmail.com>
2024-02-25 11:15:00 -0800

    3 testfile chai updated

--- unittests_in_js/3-payment.test.js
+++ unittests_in_js/3-payment.test.js
@@ -1,14 +1,14 @@
-const chai = require('chai');
 const sinon = require('sinon');
+const { expect } = require('chai'); //Import 'expect' from 'chai'
 
-const Utils = require('./utils.js');
-const sendPaymentRequestToApi = require('./3-payment.js');
-const { expect } = require('chai');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
 
 describe('sendPaymentRequestToApi function', () => {
   const spyUtil = sinon.spy(Utils, 'calculateNumber');
 
-  it('validate the usage of the Utils function', () => {
+  it('should call Utils.calculateNumber with the correct arguments and log the result', () => {
     sendPaymentRequestToApi(100, 20);
     expect(spyUtil.calledOnce).to.be.true;
     expect(spyUtil.calledWith('SUM', 100, 20)).to.be.true;
