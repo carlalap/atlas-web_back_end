8255d553974e9fdcf1ae5b49a8858e77761befea
carlalap <carlos3_alarcon@hotmail.com>
2024-02-25 13:43:39 -0800

    task6 Async tests updated

--- unittests_in_js/6-payment_token.test.js
+++ unittests_in_js/6-payment_token.test.js
@@ -1,9 +1,10 @@
 const getPaymentTokenFromAPI = require('./6-payment_token');
+
 const { expect } = require('chai');
 
 describe('getPaymentTokenFromApi', () => {
-    it('Returns promise with the object value correctly', (done) => {
-        getPaymentTokenFromAPI(True)
+    it('Returns a promise with the correct object value', (done) => {
+        getPaymentTokenFromAPI(true)
         .then((response) => {
           expect(response).to.include({ data: 'Successful response from the API' });
           done();
