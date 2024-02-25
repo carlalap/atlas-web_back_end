3a91368cb1ebb31fa6763ba8b6f6594841829227
carlalap <carlos3_alarcon@hotmail.com>
2024-02-25 10:08:57 -0800

    task5. Hooks added

---
+++ unittests_in_js/5-payment.test.js
@@ -0,0 +1,31 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+  let spyConsole;
+
+  beforeEach(() => {
+    // Create a spy for console.log
+    spyConsole = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    // Restore the stub and the spy
+    expect(spyConsole.calledOnce).to.be.true;
+    spyConsole.restore();
+  });
+
+  it('checks output arguments with 100, and 20', function() {
+    // Call the function
+    sendPaymentRequestToApi(100, 20);
+    expect(spyConsole.calledWith('The total is: 120')).to.be.true;
+    });
+
+  it('checks output arguments with 10, and 10', function() {
+    // Verify that Utils.calculateNumber was called with the correct arguments
+    sendPaymentRequestToApi(10, 10);
+    expect(spyConsole.calledWith('The total is: 20')).to.be.true;
+  });
+});
