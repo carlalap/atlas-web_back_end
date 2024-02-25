4e4a877e2cb2a5f8992199857d685307bddafb1e
carlalap <carlos3_alarcon@hotmail.com>
2024-02-25 09:29:43 -0800

    task4 updated

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -1,33 +1,21 @@
 const sinon = require('sinon');
-const expect = require('chai').expect;
-const Utils = require('./utils');
+const { expect } = require('chai');
+
 const sendPaymentRequestToApi = require('./4-payment');
+const Utils = require('./utils');
 
-describe('sendPaymentRequestToApi', function() {
-  const stubUtils;
-  const spyConsole;
+describe('Stubs', function () {
+  it('has the same math', () => {
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');
+    stubUtils.returns(10);
+    const spyConsole = sinon.spy(console, 'log');
 
-  beforeEach(() => {
-    // Create a Stub  & returns value for Utils.calculateNumber
-    stubUtils = sinon.stub(Utils, 'calculateNumber').returns(10);
-    // Create a spy for console.log
-    spyConsole = sinon.spy(console, 'log');
-  });
+    sendPaymentRequestToApi(100, 20);
 
-  afterEach(() => {
-    // Restore the stub and the spy
+    expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
+
     stubUtils.restore();
     spyConsole.restore();
-  });
-
-  it('should call Utils.calculateNumber with the correct arguments and log the result', function() {
-    // call the function
-    sendPaymentRequestToApi(100, 20);
-    // Verify that Utils.calculateNumber was called with the correct arguments
-    expect(stubUtils.calledWith('SUM', 100, 20)).to.be.true;
-     // check that the stub always return the same number 10
-    expect(stubUtils.alwaysReturned(10)).to.be.true;
-    // Verify that console.log was called with the correct message
-    expect(spyConsole.calledWith('The total is: 10')).to.be.true;
   });
 });
