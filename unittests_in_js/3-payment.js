df7f2e631d56b339f1438a4930a7caa037d1f360
carlalap <carlos3_alarcon@hotmail.com>
2024-02-25 11:18:34 -0800

    3 testfile eslint updated

--- unittests_in_js/3-payment.js
+++ unittests_in_js/3-payment.js
@@ -2,7 +2,7 @@ const Utils = require('./utils');
 
 const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
     const message = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-    console.log(`The total is: $ { message }`);
+    console.log(`The total is: ${message}`);
 };
 
 module.exports = sendPaymentRequestToApi;
