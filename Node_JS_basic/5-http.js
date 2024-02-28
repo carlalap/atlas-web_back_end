2a5d8c051c3fb61800eab3aadc825720a65b5b6d
carlalap <carlalap>
2024-02-28 12:12:51 -0600

    task5. updated

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,15 +8,17 @@ const http = require('http'); // Import the http module
 const countStudents = require('./3-read_file_async'); // Import the countStudents function from the 3-read_file_async module
 
 const port = 1245; // Set the port to 1245
+const localhost = '127.0.0.1';
 
 const app = http.createServer(async (request, response) => { // Create a new HTTP server
   response.writeHead(200, { 'Content-Type': 'text/plain' }); // Set the response header
-  if (request.url === '/') response.write('Hello Holberton School!');
-  if (request.url === '/students') { // If the request URL is '/students'
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+  } else if (request.url === '/students') { // If the request URL is '/students'
     response.write('This is the list of our students\n');
     try { // Try to fetch the list of students asynchronously
-      const data = await countStudents(process.argv[2]);
-      response.end(`${data.join('\n')}`); // Write the list of students to the response and end the response
+      const dataStudents = await countStudents(process.argv[2]);
+      response.write(`${dataStudents.join('\n')}`); // Write the list of students to the response and end the response
     } catch (error) {
       response.end(error.message);
     }
@@ -24,7 +26,7 @@ const app = http.createServer(async (request, response) => { // Create a new HTT
   response.end();
 });
 
-app.listen(port, (error) => { // Start the server and listen on the specified port
+app.listen(port, localhost, (error) => { // Start the server and listen on the specified port
   if (error) { // If an error occurs while starting the server
     console.log('Something went wrong', error); // Log the error message
   } else { // If the server starts successfully
